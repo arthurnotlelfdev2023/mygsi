6c0bc8888975da28b2d5e555678d22281bf5be3e
RofikKernelDev <rofikcoga@gmail.com>
2026-07-15 22:20:15 +0700

    Update link

--- extractRomonly.sh
+++ extractRomonly.sh
@@ -9,15 +9,12 @@ ROM_TYPE=$2
 #if [[ "$EUID" -ne 0 ]]; then
  #   exec sudo bash "$0" "$@"
 #fi
+rm -rf Tools/Firmware_extractor
 
+git clone --depth=1 --recurse-submodules \
+    https://github.com/erfanoabdi/Firmware_extractor.git \
+    Tools/Firmware_extractor
 partitions="vendor system system_ext product optics prism mi_ext my_bigball my_engineering my_manifest my_region my_carrier my_heytap my_product my_stock"
-
-if [[ -d "Tools/Firmware_extractor" ]]; then
-    echo "already..."
-else
-    echo "Cloning Firmware_extractor..."
-    git clone -q --recurse-submodules https://github.com/erfanoabdi/Firmware_extractor.git "Tools"/Firmware_extractor
-fi
 
 usage() {
   echo "Usage: $0 [rom_link] [rom_type]"
